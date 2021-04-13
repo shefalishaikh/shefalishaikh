5303973fafb358f117a61371d04619f0b79f37f1
Maaz@$&44 <shefalishaikh>
2021-04-13 23:46:41 +0530

    secondcommit

--- src/components/Splash/Design.js
+++ src/components/Splash/Design.js
@@ -5,18 +5,24 @@ import strings from '../../res/strings';
 import images from '../../res/images';
 
 const SplashDesign = () => {
+
+    function demo(){
+        alert("shdsdf");
+    }
     return(
         <View style={styles.rootView}>  
             <StatusBar backgroundColor={'white'}/>
             <View style={styles.childView}>  
                 <Image source={images.splash.icon}
                     style={styles.imageView} />
+
             </View>
             <View style={styles.footerContainer}>
                 <Text style={styles.footerView}>{strings.footer.powered_by}</Text>
                 <View style={styles.logoContainer}>
                     <Image source={images.college.logo_square}
-                    style={styles.logoContent} /> 
+                    style={styles.logoContent} 
+                    onProgress={()=>demo()}/> 
                 </View>
             </View>
         </View>
